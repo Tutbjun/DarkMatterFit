25d2eaa0229c3381dbe3e2d5bc685513f4c8c200
Tutbjün <thorbjoernclausen@gmail.com>
2025-02-25 11:53:42 +0100

    updated data projections, missing p-integral

---
+++ 3_expression.m
@@ -0,0 +1,4 @@
+M7R0
+I"ZQcx125000*I*cos(theta)/Pi^(1/2)*(exp(-I*phi)*exp(-250000000000*(f-f__A+f__d)
+^2)-exp(I*phi)*exp(-250000000000*(f+f__A-f__d)^2)+exp(I*phi)*exp(-250000000000*
+(f-f__A-f__d)^2)-exp(-I*phi)*exp(-250000000000*(f+f__A+f__d)^2))6"F$
---
+++ 6_expression.m
@@ -0,0 +1,4 @@
+M7R0
+I"ZQhw125000*I/Pi^(1/2)*(exp(-I*phi)*exp(-250000000000*(f-f__A+f__d)^2)-exp(I*p
+hi)*exp(-250000000000*(f+f__A-f__d)^2)+exp(I*phi)*exp(-250000000000*(f-f__A-f__
+d)^2)-exp(-I*phi)*exp(-250000000000*(f+f__A+f__d)^2))6"F$
